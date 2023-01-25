7c0d6851b5e057528d0a88d66273a329101b0afe
dgrushetskiy <dgrushetskiy@gmail.com>
2023-01-25 11:54:54 +0300

    create module config-srv

---
+++ config-srv/src/main/java/ru/gothmog/ws/config/ConfigSrvApplication.java
@@ -0,0 +1,15 @@
+package ru.gothmog.ws.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigSrvApplication.class, args);
+    }
+
+}
---
+++ config-srv/src/test/java/ru/gothmog/ws/config/ConfigSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
