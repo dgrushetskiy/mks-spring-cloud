b738a130b1c389c8efc620cbd842ccded659d008
dgrushetskiy <dgrushetskiy@gmail.com>
2023-02-27 00:24:45 +0300

    add documents-srv

---
+++ documents-srv/src/main/java/ru/gothmog/ws/documents/DocumentsSrvApplication.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws.documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentsSrvApplication.class, args);
+    }
+
+}
---
+++ documents-srv/src/test/java/ru/gothmog/ws/documents/DocumentsSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws.documents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsSrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
