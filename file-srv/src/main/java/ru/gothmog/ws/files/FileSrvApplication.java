5341e1743e85388ac41abe8dde1c13887b0c20c2
dgrushetskiy <dgrushetskiy@gmail.com>
2023-03-15 23:56:51 +0300

    update file-srv

--- file-srv/src/main/java/ru/gothmog/ws/files/FileSrvApplication.java
+++ file-srv/src/main/java/ru/gothmog/ws/files/FileSrvApplication.java
@@ -3,12 +3,14 @@ package ru.gothmog.ws.files;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import ru.gothmog.ws.files.core.property.FileStorageProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
         FileStorageProperties.class
 })
+@EnableDiscoveryClient
 public class FileSrvApplication {
 
     public static void main(String[] args) {
