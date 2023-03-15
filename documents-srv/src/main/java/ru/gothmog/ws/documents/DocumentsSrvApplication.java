3f5ad5c0b2176ad4c0163fb5e79888eed0484fd1
dgrushetskiy <dgrushetskiy@gmail.com>
2023-03-15 23:55:48 +0300

    update documents-srv

--- documents-srv/src/main/java/ru/gothmog/ws/documents/DocumentsSrvApplication.java
+++ documents-srv/src/main/java/ru/gothmog/ws/documents/DocumentsSrvApplication.java
@@ -2,8 +2,10 @@ package ru.gothmog.ws.documents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DocumentsSrvApplication {
 
     public static void main(String[] args) {
